bbdd8b18be971561c4bc0ac5c157842ae47e022a
Cristian Beldea <hello@capitalmerce.com>
2023-02-09 21:19:36 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+    //document.getElementById("logo").src = "/assets/images/logo-white.png";
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+    //document.getElementById("logo").src = "/assets/images/logo.png";
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            //document.getElementById("logo").src = "/assets/images/logo-white.png";
+            localStorage.setItem('color-theme', 'dark');
+            //localStorage.setItem('logo-src', '/assets/images/logo-white.png');
+
+        } else {
+            document.documentElement.classList.remove('dark');
+            //document.getElementById("logo").src = "/assets/images/logo.png";
+            localStorage.setItem('color-theme', 'light');
+            //localStorage.setItem('logo-src', '/assets/images/logo.png');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            //document.getElementById("logo").src = "/assets/images/logo.png";
+            localStorage.setItem('color-theme', 'light');
+            //ocalStorage.setItem('logo-src', '/assets/images/logo.png');
+        } else {
+            document.documentElement.classList.add('dark');
+            //document.getElementById("logo").src = "/assets/images/logo-white.png";
+            localStorage.setItem('color-theme', 'dark');
+            //localStorage.setItem('logo-src', '/assets/images/logo-white.png');
+        }
+    }
+    
+});
+
+
+
+
