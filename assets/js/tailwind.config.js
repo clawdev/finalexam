6bc1f25436e5c4558244f7f3c5af7bfd1d855333
Cristian Beldea <hello@capitalmerce.com>
2023-02-10 11:34:36 +0200

    v2

--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -2,3 +2,27 @@ module.exports = {
     darkMode: 'media',
     // ...
 }
+
+module.exports = {
+    theme: {
+      extend: {
+        typography: {
+          DEFAULT: {
+            css: {
+              color: '#666',
+              a: {
+                color: '#3182ce',
+                '&:hover': {
+                  color: '#2c5282',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    plugins: [
+      require('flowbite-typography'),
+      // ...
+    ],
+  }
