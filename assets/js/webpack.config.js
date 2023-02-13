82fb20350a4153f1eb630fb66ec5a6a71e7e6fd6
Cristian Beldea <hello@capitalmerce.com>
2023-02-13 11:31:33 +0200

    v4

---
+++ assets/js/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/favicon.ico', to: 'assets/static/images' },
+        { from: 'src/favicon.png', to: 'assets/static/images' },
+        { from: 'src/favicon.svg', to: 'assets/static/images' },
+      ],
+    }),
+  ]
+};
